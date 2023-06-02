5bc83966eab42be5d17c97eef7ee9921cf5842a3
lesyhoi111 <lesyhoi0129@gmail.com>
2023-06-02 12:12:30 +0700

    read book lsh 0206

---
+++ screens/detailBook.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, Dimensions, View } from 'react-native';
+import Pdf from 'react-native-pdf';
+
+const DetailBook = (props) => {
+    const { navigation, route } = props
+    const { idBook } = route.params
+        const source = { uri: `https://edtechbooks.org/pdfs.v2/${idBook}/_${idBook}.pdf`, cache: true };
+        console.log(`https://edtechbooks.org/pdfs.v2/${idBook}/_${idBook}.pdf`)
+        //const source = require('./test.pdf');  // ios only
+        //const source = {uri:'bundle-assets://test.pdf' };
+        //const source = {uri:'file:///sdcard/test.pdf'};
+        //const source = {uri:"data:application/pdf;base64,JVBERi0xLjcKJc..."};
+        //const source = {uri:"content://com.example.blobs/xxxxxxxx-...?offset=0&size=xxx"};
+        //const source = {uri:"blob:xxxxxxxx-...?offset=0&size=xxx"};
+
+        return (
+            <View style={styles.container}>
+                <Pdf
+                    trustAllCerts={false}
+                    source={source}
+                    enablePaging={true}
+                    onLoadComplete={(numberOfPages,filePath) => {
+                        console.log(`Number of pages: ${numberOfPages}`);
+                    }}
+                    onError={(error) => {
+                        console.log(error);
+                    }}
+                    onPressLink={(uri) => {
+                        console.log(`Link pressed: ${uri}`);
+                    }}
+                    style={styles.pdf}/>
+            </View>
+        )
+    
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 25,
+    },
+    pdf: {
+        flex:1,
+        width:Dimensions.get('window').width,
+        height:Dimensions.get('window').height,
+    }
+});
+export default DetailBook;
