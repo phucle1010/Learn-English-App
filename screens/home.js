64e7f71301e783e7871d41d6fc2e7bba48650739
Annoy1010 <lethephuc2002@gmail.com>
2023-07-04 15:04:17 +0700

    edit-home-screen

--- screens/home.js
+++ screens/home.js
@@ -292,7 +292,7 @@ const styles = StyleSheet.create({
     },
     searchcontainer: {
         flexDirection: 'row',
-        width: 306,
+        // width: 306,
         height: 44,
         borderRadius: 30,
         marginHorizontal: 20,
